3cccc6164d737427de094bced70ea96d0ec5cc98
Alexandr Ialticenco <alexandr.ialticenco@amdaris.com>
2018-02-06 12:06:54 +0200

    initial commit

---
+++ FirstApp/CookieOfLuck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    
+    class CookieOfLuck
+    {
+        private Random Random = new Random();
+        private List<String> MessageStore = new List<String>() { "You will be lucky!", "You will fail!", "Everything will be fine", "Something wrong will take place"};
+
+        public String NextMessage()
+        {
+            return MessageStore[Random.Next() % MessageStore.Count];
+        }     
+
+    }
+}
---
+++ FirstApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace FirstApp
+{
+    class Program
+    {
+
+        delegate int Squarer(int x);
+        delegate int TestSecond();
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+            Squarer d = CalcSqr;
+            d += CalcSqrt;
+            d(15);
+        }
+
+        static int CalcSqr(int x)
+        {
+            var result = x * x;
+            WriteLine($"calculating... {x} * {x} = {result}");
+            return result;
+        }
+
+        static int CalcSqrt(int x)
+        {
+            var result = Convert.ToInt32(Math.Sqrt(x));
+            WriteLine($"calculating... Sqrt ({x}) = {result}");
+            return result;
+        }
+    }
+}
